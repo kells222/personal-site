79e07c30c48aea170b92c34af5e38c4269544e32
Kelly Buckley <princesskells@Kellys-MacBook-Air.local>
2023-06-17 22:51:50 -0700

    making changes

--- javascript.js
+++ javascript.js
@@ -8,9 +8,9 @@
 const form = document.querySelector("#form")
 const submitButton = document.getElementsByClassName("submitBtn")
 const scriptURL = 'https://script.google.com/macros/s/1XTaVdkdAnzhAHoLOA-x0K4svtJOB9RUnp2OgudFCNoEE4Lo8muDKOgSO/exec'
+// const scriptURL = "https://docs.google.com/spreadsheets/d/1X4LP7NNA1M7v_3hDLSKY0audVDZ4oqmAaM4HJDqYXd8/edit#gid=0"
 
-
-form.addEventListener('onclick', e => {
+form.addEventListener('submit', e => {
   submitButton.disabled = true
   e.preventDefault()
   let requestBody = new FormData(form)
